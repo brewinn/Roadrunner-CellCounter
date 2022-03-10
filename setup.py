25aaa1ad196a7e517a326178de30b7825a737816
Brendan <brewinn@protonmail.com>
2022-03-10 16:34:23 -0600

    build: Add setup.py
    The setup.py file contains information about the package, and should
    allow the package to be installed via pip.

---
+++ setup.py
@@ -0,0 +1,9 @@
+# Following https://awaywithideas.com/the-optimal-python-project-structure/
+import setuptools
+
+with open('requirements.txt', 'r') as f:
+    install_requires = f.read().splitlines()
+
+setuptools.setup(name='cell_counter',
+                 packages=['cell_counter'],
+                 install_requires=install_requires)
