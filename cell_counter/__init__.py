ac147c4f17f1ea7991dbb4d781d879595aa980bf
Brendan <brewinn@protonmail.com>
2022-03-10 16:32:20 -0600

    build: Create __init__.py
    The __init__.py is used by python to designate the containing directory
    as part of a package.

---
+++ cell_counter/__init__.py
@@ -0,0 +1,9 @@
+# Following the recommendation in
+# https://awaywithideas.com/the-optimal-python-project-structure/,
+# we'll set a ROOT_DIR variable, and potentially include logging
+
+from os.path import dirname, abspath
+
+ROOT_DIR = dirname(abspath(__file__))
+
+## Future logging code can go here
