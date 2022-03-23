01e0f6ebe69103682b8a7d51eeff507355fbb3ac
Brendan <brewinn@protonmail.com>
2022-03-23 16:49:49 -0500

    test: Add test for multipage tiff
    Here is the first real test. It loads in the function to test, and
    checks that a loaded in tiff file has the appropriate number of pages.
    Note that the test does not yet pass, because the function is not
    implemented yet.

--- tests/test_import_tiff.py
+++ tests/test_import_tiff.py
@@ -2,10 +2,21 @@
 
 import unittest
 
+# For finding the image in the resources directory
+#  regardless of the current working directory
+import os 
+
+# Import the relevant function after installing the package
+from cell_counter.import_tiff import tiff_to_array
+
 class TestImportTiff(unittest.TestCase):
 
-    def test_fail(self):
-        self.assertFalse('start here')
+    def test_tiff_page_count(self):
+        test_dir = os.path.dirname(__file__)
+        path = test_dir + '/../resources/multipage_tiff_example.tif'
+        images = tiff_to_array(path)
+        self.assertEqual(len(images), 10)
+
 
 if __name__ == '__main__':
     unittest.main()
