702ce54f7ee25edda71dddc84f9d1ba517e80995
Brendan <brewinn@protonmail.com>
2022-03-23 19:31:46 -0500

    test: Add test for individual pixel values
    This test shows how to access a given pixel on a multipage tiff. Note
    that the access is 'image[y,x]' not 'image[x, y]'.

--- tests/test_import_tiff.py
+++ tests/test_import_tiff.py
@@ -11,11 +11,24 @@ from cell_counter.import_tiff import tiff_to_array
 
 class TestImportTiff(unittest.TestCase):
 
-    def test_tiff_page_count(self):
+    def setUp(self):
         test_dir = os.path.dirname(__file__)
         path = test_dir + '/../resources/multipage_tiff_example.tif'
-        images = tiff_to_array(path)
-        self.assertEqual(len(images), 10)
+        self.images = tiff_to_array(path)
+
+
+    def test_tiff_page_count(self):
+        self.assertEqual(len(self.images), 10)
+
+    def test_tiff_cell_values(self):
+        for index, image in enumerate(self.images):
+            #print(f'{index=}:\n{image=}')
+            self.assertEqual(image[0, 0], 255)
+            # Check for black pixel on the zero of page 10
+            #  note that the coordinates start at the top left,
+            #  and the coordinates are [y,x] rather than [x,y].
+            self.assertEqual(image[440, 550], 2 if index == 9 else 255)
+
 
 
 if __name__ == '__main__':
