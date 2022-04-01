c6a7ae0831c8b7fd0f7447fae1ae47d38541a6ff
Brendan <brewinn@protonmail.com>
2022-04-01 16:34:06 -0500

    feat: Add script to run all tests
    The run_tests.py script simply takes the scripts from the tests
    directory and runs them all. This may help with CI, if we ever get to
    that.

---
+++ run_tests.py
@@ -0,0 +1,16 @@
+# This script simply runs all available tests.
+
+if __name__ == '__main__':
+    import unittest
+
+    # For each file in the tests folder, add an import statement and an addTest statement
+    from tests.test_import_tiff import TestImportTiff
+    from tests.test_import_dataset import TestImportSyntheticDataset
+
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(TestImportTiff))
+    suite.addTest(unittest.makeSuite(TestImportSyntheticDataset))
+
+    result = unittest.TextTestRunner(verbosity=2).run(suite)
+
+    print(result)
