7ea48a00ce094b54b8259106c4f27c41d80cfdf8
Brendan <brewinn@protonmail.com>
2022-04-26 12:54:36 -0500

    feat: Add functions for filtering imported images
    Four function have been added. The get function queries the specified
    path for the images and their metadata, the save function saves the
    dataframe as a csv to the specified location, the load function loads
    the dataframe, and the final function uses a given dataframe to import
    the actual images and labels.

--- cell_counter/import_dataset.py
+++ cell_counter/import_dataset.py
@@ -4,6 +4,9 @@
 import numpy as np
 from typing import Tuple, List
 
+# For organizing and filtering the dataset
+import pandas as pd
+
 # For finding the dataset
 import os
 
@@ -13,6 +16,125 @@ from cell_counter.import_tiff import tiff_to_array, reduce_resolution
 # For random image selection
 import random
 
+def get_dataset_info(path: str = None) -> pd.DataFrame:
+    '''
+    Creates a dataframe with the properties of the dataset.
+
+    Parameters:
+    path (str): Path to image directory.
+
+    Returns:
+    pd.DataFrame: A dataframe with the dataset's properties.
+    '''
+    # Find path to images
+    if path:
+        path_to_images = path
+    else:
+        counter_dir = os.path.dirname(__file__)
+        path_to_images = counter_dir + "/../resources/BBBC005_v1_images/"
+
+    # List of found tif files
+    image_filenames = [
+        image_file
+        for image_file in os.listdir(path_to_images)
+        if image_file[-4:] == ".TIF"
+    ]
+
+    # Helper function to extract metadata from filenames
+    def extract_properties(df: pd.DataFrame):
+        df['properties']=df['image_file'].apply(lambda x: x.split('_'))
+        df['well'] = df['properties'].apply(lambda x: x[1])
+        df['cells'] = df['properties'].apply(lambda x: int(x[2][1:]))
+        df['blur'] = df['properties'].apply(lambda x: int(x[3][1:]))
+        df['sample'] = df['properties'].apply(lambda x: int(x[4][1:]))
+        df['stain'] = df['properties'].apply(lambda x: int(x[5][1]))
+        df.drop(columns=['properties'], inplace=True)
+
+    df = pd.DataFrame(image_filenames, columns=['image_file'])
+    extract_properties(df)
+    return df
+
+def save_dataset_info(df:pd.DataFrame, path: str = None):
+    '''
+    Saves the dataframe to the path, if specified.
+
+    Parameters:
+    df (pd.DataFrame): The dataframe to save.
+    path (str): Path to save the dataframe to.
+    '''
+    if path:
+        df.to_csv(path, index=False)
+    else:
+        counter_dir = os.path.dirname(__file__)
+        path_to_resources = counter_dir + "/../resources/"
+        df.to_csv(path_to_resources + 'dataset_info.csv', index=False)
+
+def load_dataset_info(path: str = None) -> pd.DataFrame:
+    '''
+    Loads dataframe from the path, if specified.
+
+    Parameters:
+    path (str): Path to load the dataframe from.
+
+    Returns:
+    pd.Dataframe: The stored dataframe.
+    '''
+    if path:
+        return pd.read_csv(path)
+    else:
+        counter_dir = os.path.dirname(__file__)
+        path_to_resources = counter_dir + "/../resources/"
+        return pd.read_csv(path_to_resources + 'dataset_info.csv')
+
+def load_images_from_dataframe(df:pd.DataFrame, split: float = 0.2, resolution: Tuple[int, int] = None,  path: str = None) -> Tuple[Tuple[np.ndarray, np.ndarray], Tuple[np.ndarray, np.ndarray]]:
+    '''
+    Uses the 'image_file' column to load images from the path.
+
+    Parameters:
+    df (pd.Dataframe): The dataframe to reference for loading images. It is
+    assumed that all entries in the dataframe should be loaded.
+    split (float): Proportion of images to return as testing data.
+    resolution (Tuple[int,int]): Resolution to reduce images down to.
+    path (str): Path to the directory to load the images from.
+
+    Returns:
+    Tuple[Tuple[np.ndarray, np.ndarray], Tuple[np.ndarray, np.ndarray]]:
+    A set of images and labels for the training and testing sets, respectively.
+    '''
+
+    # Find the path to the images
+    if path:
+        path_to_images = path
+    else:
+        counter_dir = os.path.dirname(__file__)
+        path_to_images = counter_dir + "/../resources/BBBC005_v1_images/"
+
+    # Load images and labels based on df
+    image_names = df['image_file'].tolist()
+    labels = df['cells'].tolist()
+    images = []
+    for image in image_names:
+        image = tiff_to_array(path_to_images + image)[0]
+        if resolution != None:
+            image = reduce_resolution(image, resolution)
+        images.append(image)
+
+    # Determine the number of testing and training samples
+    num = df.shape[0]
+    num_test = int(num * split)
+    if num_test < 0:
+        num_test = 0
+    elif num_test > num:
+        num_test = num
+    num_training = num - num_test
+
+    # Split samples and labels into test and training sets
+    training_samples = np.array(images[:num_training])
+    training_labels = np.array(labels[:num_training])
+    testing_samples = np.array(images[num_training:])
+    testing_labels = np.array(labels[num_training:])
+
+    return (training_samples, training_labels), (testing_samples, testing_labels)
 
 def load_synthetic_dataset(
     is_random: bool = True,
