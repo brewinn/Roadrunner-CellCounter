87f8016b5e70dc68096b510a5b547f1e0f4c9d62
alan <alancabrera10@gmail.com>
2022-04-29 15:41:33 -0700

    added comments and implemented suggestion by Brendan

--- cell_counter/unet.py
+++ cell_counter/unet.py
@@ -11,15 +11,17 @@ is a kind of neural network model designed
 to work with two-dimensional image data.
     It makes use of a convolutional layer that 
 gives the network its name. This layer 
-performs an operation called a convolution 
+performs an operation called a convolution, 
 which is essentially taking the dot product
 of a set of weights, or filters, and an
 array derived from an input image.
-    Concatenation is what makes segmentation
-possible.
+    U-Net, introduced in 2015, was an innovative
+approach to addressing the issue of image
+segmentation...
 
-tensorflow is a machine learning library
-keras is a neaural network library
+We use tensorflow, a machine learning library,
+and keras, a neaural network library, to help
+make it possible.
 """
 # For type hinting
 from typing import List, Tuple
@@ -33,12 +35,13 @@ from cell_counter.import_dataset import get_dataset_info, load_images_from_dataf
 # For creating and using CNN
 import tensorflow as tf
 
-#For unet
+# For unet
 from keras.models import *
 from keras.layers import *
 from keras.optimizers import *
 from keras.callbacks import ModelCheckpoint, LearningRateScheduler
 from keras import backend as keras
+
 
 def unet_preprocess_data(path: str = None, num: int = 2500):
     """
@@ -56,13 +59,15 @@ def unet_preprocess_data(path: str = None, num: int = 2500):
     """
     # Filter to only use images without blur
     df = get_dataset_info(path)
-    df = df[df['blur']==1]
+    df = df[df['blur'] == 1]
 
     # Randomly select 'num' from the remaining images, without replacement
     df = df.sample(n=num, replace=False)
 
     # Return images and labels from dataframe
-    (training_images, training_labels), ( testing_images, testing_labels,) = load_images_from_dataframe(df, path=path, resolution=(128, 128))
+    (training_images, training_labels), (
+        testing_images, testing_labels,
+        ) = load_images_from_dataframe(df, path=path, resolution=(128, 128))
 
     scale = 1 / float(255)
     for index, image in enumerate(training_images):
@@ -74,6 +79,9 @@ def unet_preprocess_data(path: str = None, num: int = 2500):
         testing_images,
         testing_labels,
     )
+# The original U-Net architecture,
+# from https://github.com/zhixuhao/unet/blob/master/model.py
+
 
 def build_unet():
     """
@@ -86,8 +94,103 @@ def build_unet():
 
     preprocessed_image_shape = (128, 128, 1)
 
-    #referred to https://github.com/zhixuhao/unet/blob/master/model.py
-    inputs = Input(pretrained_weights=None, input_size=preprocessed_image_shape)
+    inputs = Input(pretrained_weights=None,
+                   input_size=preprocessed_image_shape)
+    conv1 = Conv2D(64, 3, activation='relu', padding='same',
+                   kernel_initializer='he_normal')(inputs)
+    conv1 = Conv2D(64, 3, activation='relu', padding='same',
+                   kernel_initializer='he_normal')(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+    # =========================================================================
+    conv2 = Conv2D(128, 3, activation='relu', padding='same',
+                   kernel_initializer='he_normal')(pool1)
+    conv2 = Conv2D(128, 3, activation='relu', padding='same',
+                   kernel_initializer='he_normal')(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+    # =========================================================================
+    conv3 = Conv2D(256, 3, activation='relu', padding='same',
+                   kernel_initializer='he_normal')(pool2)
+    conv3 = Conv2D(256, 3, activation='relu', padding='same',
+                   kernel_initializer='he_normal')(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+    # =========================================================================
+    conv4 = Conv2D(512, 3, activation='relu', padding='same',
+                   kernel_initializer='he_normal')(pool3)
+    conv4 = Conv2D(512, 3, activation='relu', padding='same',
+                   kernel_initializer='he_normal')(conv4)
+    drop4 = Dropout(0.5)(conv4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
+    # =========================================================================
+    conv5 = Conv2D(1024, 3, activation='relu', padding='same',
+                   kernel_initializer='he_normal')(pool4)
+    conv5 = Conv2D(1024, 3, activation='relu', padding='same',
+                   kernel_initializer='he_normal')(conv5)
+    drop5 = Dropout(0.5)(conv5)
+    # =========================================================================
+    up6 = Conv2D(512, 2, activation='relu', padding='same',
+                 kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(drop5))
+    merge6 = concatenate([drop4, up6], axis=3)
+    conv6 = Conv2D(512, 3, activation='relu', padding='same',
+                   kernel_initializer='he_normal')(merge6)
+    conv6 = Conv2D(512, 3, activation='relu', padding='same',
+                   kernel_initializer='he_normal')(conv6)
+    # =========================================================================
+    up7 = Conv2D(256, 2, activation='relu', padding='same',
+                 kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(conv6))
+    merge7 = concatenate([conv3, up7], axis=3)
+    conv7 = Conv2D(256, 3, activation='relu', padding='same',
+                   kernel_initializer='he_normal')(merge7)
+    conv7 = Conv2D(256, 3, activation='relu', padding='same',
+                   kernel_initializer='he_normal')(conv7)
+    # =========================================================================
+    up8 = Conv2D(128, 2, activation='relu', padding='same',
+                 kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(conv7))
+    merge8 = concatenate([conv2, up8], axis=3)
+    conv8 = Conv2D(128, 3, activation='relu', padding='same',
+                   kernel_initializer='he_normal')(merge8)
+    conv8 = Conv2D(128, 3, activation='relu', padding='same',
+                   kernel_initializer='he_normal')(conv8)
+    # =========================================================================
+    up9 = Conv2D(64, 2, activation='relu', padding='same',
+                 kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(conv8))
+    merge9 = concatenate([conv1, up9], axis=3)
+    conv9 = Conv2D(64, 3, activation='relu', padding='same',
+                   kernel_initializer='he_normal')(merge9)
+    conv9 = Conv2D(64, 3, activation='relu', padding='same',
+                   kernel_initializer='he_normal')(conv9)
+    conv9 = Conv2D(2, 3, activation='relu', padding='same',
+                   kernel_initializer='he_normal')(conv9)
+    # =========================================================================
+    conv10 = Conv2D(1, 1, activation='sigmoid')(conv9)
+
+    model = Model(inputs=inputs, outputs=conv10)
+
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-4),
+                  loss=tf.keras.losses.BinaryCrossentropy(),
+                  metrics=['accuracy'])
+
+    # model.summary()
+
+    if(pretrained_weights):
+        model.load_weights(pretrained_weights)
+
+    return model
+# The modified U-Net architecture,
+# based on https://github.com/zhixuhao/unet/blob/master/model.py
+
+
+def build_modified_unet():
+    """
+    Returns a unet model for use on a preprocessed dataset.
+
+    Returns:
+    keras Model class: The generated U-Net codel.
+
+    """
+
+    preprocessed_image_shape = (128, 128, 1)
+
+    inputs = Input(preprocessed_image_shape)
     conv1 = Conv2D(64, 3, activation='relu', padding='same',
                    kernel_initializer='he_normal')(inputs)
     conv1 = Conv2D(64, 3, activation='relu', padding='same',
@@ -131,38 +234,33 @@ def build_unet():
                    kernel_initializer='he_normal')(merge7)
     conv7 = Conv2D(256, 3, activation='relu', padding='same',
                    kernel_initializer='he_normal')(conv7)
+    
+    # =========================================================================
+    # Append simple cnn to reduce the final output to appropriate dimensions as 
+    # suggested by my team leader, Brendan Winn. Implementated in 
+    # nalu_fcrn_cellcounter.py by Brendan Winn. Modified to fit U-Net.
+    
+    cnn_filter = 16
+    cnn_pool_size = 2
+    kernal = (3, 3)
+    dropout_rate = 0.2
 
-    up8 = Conv2D(128, 2, activation='relu', padding='same',
-                 kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(conv7))
-    merge8 = concatenate([conv2, up8], axis=3)
-    conv8 = Conv2D(128, 3, activation='relu', padding='same',
-                   kernel_initializer='he_normal')(merge8)
-    conv8 = Conv2D(128, 3, activation='relu', padding='same',
-                   kernel_initializer='he_normal')(conv8)
+    outputs_ = Conv2D(cnn_filter, kernal, activation="relu")(conv7)
+    outputs_ = MaxPooling2D(pool_size=cnn_pool_size)(outputs_)
+    outputs_ = Dropout(dropout_rate)(outputs_)
+    outputs_ = Conv2D(
+        2 * cnn_filter, kernal, activation="relu")(outputs_)
+    outputs_ = MaxPooling2D(pool_size=cnn_pool_size)(outputs_)
+    outputs_ = Conv2D(
+        2 * cnn_filter, kernal, activation="relu")(outputs_)
 
-    up9 = Conv2D(64, 2, activation='relu', padding='same',
-                 kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(conv8))
-    merge9 = concatenate([conv1, up9], axis=3)
-    conv9 = Conv2D(64, 3, activation='relu', padding='same',
-                   kernel_initializer='he_normal')(merge9)
-    conv9 = Conv2D(64, 3, activation='relu', padding='same',
-                   kernel_initializer='he_normal')(conv9)
-    conv9 = Conv2D(2, 3, activation='relu', padding='same',
-                   kernel_initializer='he_normal')(conv9)
-    conv10 = Conv2D(1, 1, activation='sigmoid')(conv9)
-
-    model = Model(inputs=inputs, outputs=conv10)
-
-    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-4),
-                  loss=tf.keras.losses.BinaryCrossentropy(),
-                   metrics=['accuracy'])
-
-    # model.summary()
-
-    if(pretrained_weights):
-        model.load_weights(pretrained_weights)
-
+    outputs_ = Flatten()(outputs_)
+    outputs_ = Dense(64)(outputs_)
+    outputs_ = Dense(1, activation="relu")(outputs_)
+    
+    model = Model(input=inputs, output = outputs_)
     return model
+
 
 def compile_unet(model):
     """
@@ -244,7 +342,7 @@ def run_unet(
 
 
 if __name__ == "__main__":
-    model = build_unet()
+    model = build_modified_unet()
     compile_unet(model)
 
     training_hist, _ = run_unet(
